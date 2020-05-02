87746fb6146c04a31cb1e72377cc02f44bfcffec
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-05-02 21:48:56 +0900

    initial commit

---
+++ plugins/check_diff_py.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Diff based python style checker plugin for DiffSentinel
+#
+# This plugin needs to install "pycodestyle"
+#
+# Author: Atsushi Sakai (@Atsushi_twi)
+#
+
+#######################################
+# check diff for .py file
+# Globals:
+#   GIT_ROOT_DIR
+# Arguments:
+#   branch commit
+# Outputs:
+#   None
+#######################################
+function check_diff_py(){
+    echo "start check_diff_py"
+    cd ${GIT_ROOT_DIR}
+    local branch_commit=$1
+    git diff -U0 ${branch_commit} -- '*'.py | pycodestyle --diff
+    check_result=$?
+    if [[ ${check_result} -ne 0 ]];
+    then
+        echo "Error: Your changes contain pycodestyle errors."
+        exit 1
+    fi
+    cd $0 # move back to the script dir
+}
